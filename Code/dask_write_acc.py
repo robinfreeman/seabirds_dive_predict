66966e4f0c5bbeb57a8a3018c0a6a46059d15052
Luke Swaby <lds20@ic.ac.uk>
2021-05-17 18:10:38 +0100

    Processes all files. Compresses to npz instead of npy.

--- Code/dask_write_acc.py
+++ Code/dask_write_acc.py
@@ -14,6 +14,16 @@ import re
 import dask.dataframe as dd
 import dask.array as da
 
+import time
+
+import os
+import pickle
+import uuid
+import numpy as np
+import dask.core as core
+from dask.highlevelgraph import HighLevelGraph
+from dask.base import compute_as_if_collection
+
 ## Functions ##
 
 def check_for_dive(arr, thrshold=0.5):
@@ -26,6 +36,93 @@ def check_for_dive(arr, thrshold=0.5):
     """
     return int((arr[~da.isnan(arr)] > thrshold).any())
 
+
+def to_npz_stack(dirname, x, axis=0):
+    """
+    Write dask array to a stack of .npz files
+
+    This partitions the dask.array along one axis and stores each block along
+    that axis as a single compressed .npz file in the specified directory
+
+    Modified from the dask function: to_npy_stack()
+
+    :param dirname:
+    :param x:
+    :param axis:
+    :return:
+    """
+
+    chunks = tuple((c if i == axis else (sum(c),)) for i, c in enumerate(x.chunks))
+    xx = x.rechunk(chunks)
+
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    meta = {"chunks": chunks, "dtype": x.dtype, "axis": axis}
+
+    with open(os.path.join(dirname, "info"), "wb") as f:
+        pickle.dump(meta, f)
+
+    # I want it to save files named birdID.npz containing all separate arrays
+    # to specified dir
+    name = "to-npy-stack-" + str(uuid.uuid1())  # create universally unique ID
+    dsk = {
+        (name, i): (np.savez_compressed, os.path.join(dirname, "%d.npz" % i), key)
+        for i, key in enumerate(core.flatten(xx.__dask_keys__()))
+    }
+
+    graph = HighLevelGraph.from_collections(name, dsk, dependencies=[xx])
+    compute_as_if_collection(da.Array, graph, list(dsk))
+
+    return meta
+
+def to_npz_stack_other(dirname, x, axis=0):
+    """
+    Write dask array to a stack of .npz files
+
+    This partitions the dask.array along one axis and stores each block along
+    that axis as a single compressed .npz file in the specified directory
+
+    Modified from the dask function: to_npy_stack()
+
+    :param dirname:
+    :param x:
+    :param axis:
+    :return:
+    """
+    chunks = tuple((c if i == axis else (sum(c),)) for i, c in enumerate(x.chunks))
+    xx = x.rechunk(chunks)
+
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    meta = {"chunks": chunks, "dtype": x.dtype, "axis": axis}
+
+    with open(os.path.join(dirname, "info"), "wb") as f:
+        pickle.dump(meta, f)
+
+    # I want it to save files named birdID.npz containing all separate arrays
+    # to specified dir
+    name = "to-npy-stack-" + str(uuid.uuid1())  # create universally unique ID
+    dsk = {
+        (name, i): (np.savez_compressed, os.path.join(dirname, "%d.npz" % i), key)
+        for i, key in enumerate(core.flatten(xx.__dask_keys__()))
+    }
+
+    graph = HighLevelGraph.from_collections(name, dsk, dependencies=[xx])
+    compute_as_if_collection(da.Array, graph, list(dsk))
+
+    # dsk1 = {
+    #    name : (np.savez_compressed,
+    #           os.path.join(dirname, birdie),
+    #           tuple(core.flatten(xx.__dask_keys__()))
+    #           )
+    # }
+    #
+    #graph1 = HighLevelGraph._from_collection(name, dsk1, collection=xx)
+    #compute_as_if_collection(da.Array, graph1, list(dsk1))
+
+    return meta
 
 def main(arr, birdID, wdw=250):
     """
@@ -46,15 +143,13 @@ def main(arr, birdID, wdw=250):
     d = da.apply_along_axis(check_for_dive, 1, depth)
     train_data = da.hstack((x, y, z, d.reshape((d.shape[0], 1))))
 
+    meta = to_npz_stack(f'../Data/Acc/{birdID}', train_data, axis=0)
+
     # Informative output stats
-    shp = train_data.shape
-    no_files = len(train_data.chunks[0])
-    #for i in range(len(train_data.chunks[0])):
-    #    print(f'\t{(train_data.chunks[0][i], sum(train_data.chunks[1]))}')
+    meta_out = {'shape': train_data.shape, 'files': len(train_data.chunks[0])}
+    meta_out.update(meta)
 
-    da.to_npy_stack(f'../Data/Acc/{birdID}', train_data, axis=0)
-
-    return shp, no_files
+    return meta_out
 
 ## Main ##
 
@@ -70,19 +165,29 @@ if __name__ == '__main__':
     # Grab list of reevant file paths
     files = glob.glob(f'{pth}*1.csv')
 
-    for file in files:
+    count = 0
 
-        print(f'Processing file {file}...')
+    for no, file in enumerate(files, 1):
+
+        tik = time.time()
+
+        print(f"PROCESSING FILE {no}: '{file}'...")
         arr = dd.read_csv(file, usecols=['X', 'Y', 'Z', 'Depth']).to_dask_array(
             lengths=True)
 
-        #arr = arr[:2500]
+        #arr = arr[:2500000]
 
         # Grab bird ID from filepath
         bird = re.search(r"/(\w+).csv", file).group(1)
+        meta = main(arr, birdID=bird)
 
-        shp, no_files = main(arr, birdID=bird)
+        # Informative output
+        for k, v in meta.items():
+            print('\t' + k + ':', v)
 
-        print(f' - Saved array of shape {shp} to {no_files} .npy files')
+        tok = time.time() - tik
 
-    sys.exit('\nDone')
+        print("Time elapsed: %.2fs" % tok)
+        count += tok
+
+    sys.exit('\nDone\n\nTotal time elapsed: %.2fs' % count)
