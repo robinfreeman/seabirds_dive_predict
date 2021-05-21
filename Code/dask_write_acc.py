9b5efafecbdb60406a5ebe56488ea92847e9b2f6
Luke Swaby <lds20@ic.ac.uk>
2021-05-21 17:16:49 +0100

    Attempting to train model iteratively

--- Code/dask_write_acc.py
+++ Code/dask_write_acc.py
@@ -8,21 +8,19 @@ __version__ = '0.0.1'
 
 ## Imports ##
 
+import os #
+import pickle #
+import uuid #
 import sys
 import glob
 import re
+import time
+import numpy as np #
 import dask.dataframe as dd
 import dask.array as da
-
-import time
-
-import os
-import pickle
-import uuid
-import numpy as np
-import dask.core as core
-from dask.highlevelgraph import HighLevelGraph
-from dask.base import compute_as_if_collection
+import dask.core as core #
+from dask.highlevelgraph import HighLevelGraph #
+from dask.base import compute_as_if_collection #
 
 ## Functions ##
 
@@ -63,8 +61,6 @@ def to_npz_stack(dirname, x, axis=0):
     with open(os.path.join(dirname, "info"), "wb") as f:
         pickle.dump(meta, f)
 
-    # I want it to save files named birdID.npz containing all separate arrays
-    # to specified dir
     name = "to-npy-stack-" + str(uuid.uuid1())  # create universally unique ID
     dsk = {
         (name, i): (np.savez_compressed, os.path.join(dirname, "%d.npz" % i), key)
@@ -76,55 +72,7 @@ def to_npz_stack(dirname, x, axis=0):
 
     return meta
 
-def to_npz_stack_other(dirname, x, axis=0):
-    """
-    Write dask array to a stack of .npz files
-
-    This partitions the dask.array along one axis and stores each block along
-    that axis as a single compressed .npz file in the specified directory
-
-    Modified from the dask function: to_npy_stack()
-
-    :param dirname:
-    :param x:
-    :param axis:
-    :return:
-    """
-    chunks = tuple((c if i == axis else (sum(c),)) for i, c in enumerate(x.chunks))
-    xx = x.rechunk(chunks)
-
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-
-    meta = {"chunks": chunks, "dtype": x.dtype, "axis": axis}
-
-    with open(os.path.join(dirname, "info"), "wb") as f:
-        pickle.dump(meta, f)
-
-    # I want it to save files named birdID.npz containing all separate arrays
-    # to specified dir
-    name = "to-npy-stack-" + str(uuid.uuid1())  # create universally unique ID
-    dsk = {
-        (name, i): (np.savez_compressed, os.path.join(dirname, "%d.npz" % i), key)
-        for i, key in enumerate(core.flatten(xx.__dask_keys__()))
-    }
-
-    graph = HighLevelGraph.from_collections(name, dsk, dependencies=[xx])
-    compute_as_if_collection(da.Array, graph, list(dsk))
-
-    # dsk1 = {
-    #    name : (np.savez_compressed,
-    #           os.path.join(dirname, birdie),
-    #           tuple(core.flatten(xx.__dask_keys__()))
-    #           )
-    # }
-    #
-    #graph1 = HighLevelGraph._from_collection(name, dsk1, collection=xx)
-    #compute_as_if_collection(da.Array, graph1, list(dsk1))
-
-    return meta
-
-def main(arr, birdID, wdw=250):
+def main(arr, birdID, outdir, wdw=250):
     """
     Creates numpy dataframe by taking a rolling window of 250 rows of the input
     arr and horizontally arranging x, y, and z values followed by a binary
@@ -135,6 +83,8 @@ def main(arr, birdID, wdw=250):
     :param arr: numpy array containing 4 columns: X, Y, Z, and Depth
     :return:
     """
+    assert outdir.endswith('/'), "outdir argument string must end with a '/'"
+
     x = da.lib.stride_tricks.sliding_window_view(arr[:, 0], wdw)
     y = da.lib.stride_tricks.sliding_window_view(arr[:, 1], wdw)
     z = da.lib.stride_tricks.sliding_window_view(arr[:, 2], wdw)
@@ -143,7 +93,7 @@ def main(arr, birdID, wdw=250):
     d = da.apply_along_axis(check_for_dive, 1, depth)
     train_data = da.hstack((x, y, z, d.reshape((d.shape[0], 1))))
 
-    meta = to_npz_stack(f'../Data/Acc/{birdID}', train_data, axis=0)
+    meta = to_npz_stack(f'{outdir}{birdID}', train_data, axis=0)
 
     # Informative output stats
     meta_out = {'shape': train_data.shape, 'files': len(train_data.chunks[0])}
@@ -155,12 +105,15 @@ def main(arr, birdID, wdw=250):
 
 if __name__ == '__main__':
     # check inputs
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         pth = '../Data/BIOT_DGBP/BIOT_DGBP/'
+        outdir = '../Data/Acc/'
         print(f"WARNING: incorrect number of args provided. Please provide "
-              f"directory path as a single argument.\nDefault used: '{pth}'\n")
+              f"2 directory paths:\n1. Path to input files.\n2. Path to output."
+              f"\nDefaults used:\nInput: '{pth}'\nOutput: {outdir}")
     else:
         pth = sys.argv[1]
+        outdir = sys.argv[2]
 
     # Grab list of reevant file paths
     files = glob.glob(f'{pth}*1.csv')
@@ -179,7 +132,7 @@ if __name__ == '__main__':
 
         # Grab bird ID from filepath
         bird = re.search(r"/(\w+).csv", file).group(1)
-        meta = main(arr, birdID=bird)
+        meta = main(arr, birdID=bird, outdir=outdir)
 
         # Informative output
         for k, v in meta.items():
